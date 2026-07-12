6be720ded356c6cd3f986dffea075178a2504ea9
Abhisek <abhisekpattanayak07@gmail.com>
2026-07-12 15:24:49 +0530

    frontend: initialization done

---
+++ client/src/components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Brain } from 'lucide-react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <Brain />
+        </div>
+    </div>
+  )
+}
